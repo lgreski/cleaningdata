2fd9d0355b14f227ea3c36bed69b3ebadd0d305a
Len Greski <leonard.m@greski.com>
2015-08-10 07:40:04 -0700

    Initialize analysis R script for course project.

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## File: run_analysis.r
+## Author: Leonard Greski
+## Course: getdata-031, Getting and Cleaning Data, Johns Hopkins University / Coursera
+## 
+## Date: 10 August 2015
+## Revision history: revisions tracked on github.com
+## 
