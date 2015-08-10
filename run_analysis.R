72f664d0d6af6f63b85f4b59317a1b1895d9f971
lgreski <len@ageretechnology.com>
2015-08-10 16:57:33 -0700

    Added code to drop unnecessary variables and merge test with train data

--- run_analysis.R
+++ run_analysis.R
@@ -12,24 +12,55 @@
 ## 4. Appropriately labels the data set with descriptive variable names. 
 ## 5. From the data set in step 4, creates a second, independent tidy data set 
 ##    with the average of each variable for each activity and each subject.
-if (Sys.getenv("OS") == "Windows_NT") setwd("C:/Users/Leonard/Desktop/OneDrive/Coursera/R Working Directory")
+## if (Sys.getenv("OS") == "Windows_NT") setwd("C:/Users/Leonard/Desktop/OneDrive/Coursera/R Working Directory")
 
 ## set up directories
 theTrainDirectory <- "./data/UCI HAR Dataset/train/"
 theTestDirectory <- "./data/UCI HAR Dataset/test/"
 theDataDirectory <- "./data/UCI HAR Dataset/"
+library(dplyr)
 
 ## read activity file
-activityData <- read.table(paste(theDataDirectory,"activity_labels.txt",sep=''))
-## read features file
-featureData <- read.table(paste(theDataDirectory,"features.txt",sep=''))
+activityData <- read.table(paste(theDataDirectory,"activity_labels.txt",sep=''),
+                           col.names=c("activityid","activityname"),stringsAsFactors=FALSE)
 ## read test file
-testData <- read.table(paste(theTestDirectory,"x_test.txt",sep=""))
-testLabels <- read.table(paste(theTestDirectory,"y_test.txt",sep=""))
-testSubjects <- read.table(paste(theTestDirectory,"subject_test.txt",sep=""))
+testData <- read.table(paste(theTestDirectory,"X_test.txt",sep=""))
+testLabels <- read.table(paste(theTestDirectory,"y_test.txt",sep=""),
+                         col.names=c("activityId"))
+testSubjects <- read.table(paste(theTestDirectory,"subject_test.txt",sep=""),col.names=c("personId"))
 
-## identify the columns that measure means from the feature data - as an array of indexes 
-theMeans <- featureData[grepl("mean()",featureData$V2) & !grepl("meanFreq()",featureData$V2),1]
+## read features file
+featureData <- read.table(paste(theDataDirectory,"features.txt",sep=''),stringsAsFactors=FALSE)
 
-## identify the columns that measure standard deviations from the feature data
-theStds <- featureData[grep("std()",featureData$V2),1]
+## identify the columns that measure means from the feature data as vector of row indexes
+theMeanIndexes <- featureData[grepl("mean()",featureData$V2) & !grepl("meanFreq()",featureData$V2),1]
+
+## identify the columns that measure standard deviations from the feature data as vector
+## of row indexes
+theStdsIndexes <- featureData[grep("std()",featureData$V2),1]
+
+## process featureData to clean up column names and apply as column names to test data 
+featureData[,2] <- sub("mean","Mean",featureData[,2])
+featureData[,2] <- sub("std","Stdev",featureData[,2])
+## strip out () from featureData
+featureData[,2] <- sub("\\(\\)","",featureData[,2])
+## strip out dashes, and label the testData
+featureData[,2] <- gsub("-","",featureData[,2])
+colnames(testData) <- featureData[,2]
+meanColNames <- featureData[c(theMeanIndexes,theStdsIndexes),2]
+theTestTable <- as.tbl(testData)
+theDataSubset <- theTestTable[,meanColNames]
+## bind test subject ids, activity ids to test data 
+testDataFinal <- cbind(testSubjects,testLabels,theDataSubset)
+## read training data files
+trainData <- read.table(paste(theTrainDirectory,"X_train.txt",sep=""))
+trainLabels <- read.table(paste(theTrainDirectory,"y_train.txt",sep=""),
+                         col.names=c("activityId"))
+trainSubjects <- read.table(paste(theTrainDirectory,"subject_train.txt",sep=""),col.names=c("personId"))
+theTrainTable <- as.tbl(trainData)
+trainDataSubset <- theTrainTable[,meanColNames]
+trainDataFinal <- cbind(trainSubjects,trainLabels,trainDataSubset)
+## remove unnecessary data prior to rbind
+## rm(testData,testLabels,testSubjects,theDataSubset,theTestTable,trainData,theTrainTable)
+## bind the rows, should be 2947 + 7352 = 10299 rows, 68 columns 
+theData <- rbind(trainDataFinal,testDataFinal)
