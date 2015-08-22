7438e001190cb504e34a5d5682793ffbebd26e83
lgreski <len@ageretechnology.com>
2015-08-22 16:26:34 -0700

    corrected typo on project instructions comment line

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@
 ## Date: 10 August 2015
 ## Revision history: revisions tracked on github.com
 ## 
-## Processing requirements per Project Instructions at 
+## Processing requirements per Project Instructions
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 ## 3. Uses descriptive activity names to name the activities in the data set
